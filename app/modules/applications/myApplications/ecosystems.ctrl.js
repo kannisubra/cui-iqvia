a89d4afe242a929a6df17480f29b286f66415068
COVISINT\ksubramanian <ksubramanian@opentext.com>
2018-03-20 20:03:57 -0400

    ecosystem,subject,device

--- app/modules/applications/myApplications/ecosystems.ctrl.js
+++ app/modules/applications/myApplications/ecosystems.ctrl.js
@@ -17,14 +17,44 @@ angular.module('applications')
             .then(res=>{
                     ecosystems.groupTemplateDetails2 = res;
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].name[0].text);
+            console.log("kanni"+ecosystems.groupTemplateDetails2[0].name[0].text);
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[0].attributeType.name);
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[0].value);
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[1].attributeType.name);
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[1].value);
             console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[2].attributeType.name);
-            console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[2].value);            
+            console.log("kanni"+ecosystems.groupTemplateDetails2[0].attributes[2].value);
+            API.cui.getGroupMemberships({groupId:ecosystems.groupTemplateDetails2[0].id})
+            .then(res=>{
+                    ecosystems.groupMembers = res;
+            console.log("kanni member id is"+ecosystems.groupMembers[0].member.id);
+            
+            API.cui.getGroupMemberships({groupId:ecosystems.groupMembers[0].member.id})
+            .then(res=>{
+                    ecosystems.groupMembers2 = res;
+            console.log("kanni member id is"+ecosystems.groupMembers2[0].member.id);
+            console.log("kanni member id is"+ecosystems.groupMembers2[1].member.id);
+            
+            API.cui.getGroupMemberships({groupId:ecosystems.groupMembers2[1].member.id})
+            .then(res=>{
+                    ecosystems.groupMembers3 = res;
+            console.log("kanni member id is"+ecosystems.groupMembers3[0].group.name[0].text);
+                }).fail(err=>{
+                        console.log(err);
+
+            })            
+                }).fail(err=>{
+                        console.log(err);
+
+            }) 
+                }).fail(err=>{
+                        console.log(err);
+
+                }) 
         }).fail(err=>{
                 console.log(err);
                
         }) 
+        
+        
 })
